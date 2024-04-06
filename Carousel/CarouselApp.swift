e958ae03a729da0121470251f78bb5e8e72dc6fd
Prahlad <prahlad.dhungana@aircanada.ca>
2024-04-06 20:38:29 -0300

    Initial Commit

---
+++ Carousel/CarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselApp.swift
+//  Carousel
+//
+//  Created by Prahlad Dhungana on 2024-04-06.
+//
+
+import SwiftUI
+
+@main
+struct CarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Carousel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Carousel
+//
+//  Created by Prahlad Dhungana on 2024-04-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
